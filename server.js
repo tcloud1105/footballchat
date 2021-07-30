335d36ab44392b6769415efdd8a89a3804c17440
don savinero <donsavinero@gmail.com>
2021-07-30 19:59:39 +0200

    Express Configuration Function

--- server.js
+++ server.js
@@ -14,14 +14,24 @@ container.resolve(function(users){
         server.listen(3000, function(){
             console.log("Listening on port 3000");
         })   
+        
+        configureExpress(app);
+        
+        //Setup Router
+        const router = require('express-promise-router')();
+        users.setRouting(router);
+    
+        app.use(router);
     }
     
-    //Setup Router
-    const router = require('express-promise-router')();
-    users.setRouting(router);
     
     
-    app.use(router);
+    function configureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine','ejs');
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended:true}));
+    }
 });
 
 
