b03a3d0dc04e1aadad6fde662e262e3ce1e8a688
don savinero <donsavinero@gmail.com>
2021-07-31 20:54:28 +0200

    Signup Get Route

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,14 @@ module.exports = function(_){
     return{
         setRouting:function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.signUpPage);
         },
                        
         indexPage:function(req,res){
-                return res.render('index',{title:"Index Page"})
+                return res.render('index',{title:"HomePage"})
+        },
+        signUpPage:function(req,res){
+            return res.render('signup',{title:'Sign Up'})
         }
     }
 }
--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const http = require('http');
 const cookieParser = require('cookie-parser');
 const validator = require('express-validator');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
-const flash = require('flash');
+const flash = require('connect-flash');
 const passport = require('passport');
 
 
 const container = require('./container');
 
 container.resolve(function(users){
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost:27017/footballchat',{useMongoClient:true});
+    //mongoose.Promise = global.Promise;
+   // mongoose.connect('mongodb://localhost:27017/footballchat');
+    //mongoose.connect('mongodb://localhost:27017/footballchat',{useMongoClient:true});
     
    const app = setUpExpress(); 
     
@@ -47,13 +48,13 @@ container.resolve(function(users){
         app.use(bodyParser.json())
         app.use(bodyParser.urlencoded({extended:true}));
         
-        app.use(validator());
-        app.use(session({
-            secret:'thisisasecretkey',
-            resave:true,
-            saveInitialized:true,
-            store: new MongoStore(mongooseConnection:mongoose.connection)
-        }))
+        
+//        app.use(session({
+//            secret:'thisisasecretkey',
+//            resave:true,
+//            saveInitialized:true,
+//            store: new MongoStore({mongooseConnection:mongoose.connection})
+//        }))
         
         app.use(flash());
         app.use(passport.initialize());
