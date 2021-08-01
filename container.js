977790f2ce23b44ca69e7fe8277ae766f8d6601f
don savinero <donsavinero@gmail.com>
2021-08-01 20:30:34 +0200

    Post sign up

--- container.js
+++ container.js
@@ -3,7 +3,8 @@ const path = require('path');
 const container = dependable.container();
 
 const simpleDependencies = [
-    ['_','lodash']
+    ['_','lodash'],
+    ['passport','passport']
 ];
 
 simpleDependencies.forEach(function(val){
--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,30 @@
 'use strict'
 
-module.exports = function(_){
+module.exports = function(_,passport){
     return{
         setRouting:function(router){
             router.get('/', this.indexPage);
             router.get('/signup', this.signUpPage);
+            router.get('/home',this.homePage)
+            
+            
+            router.post('/signup',this.postSignUp);
         },
                        
         indexPage:function(req,res){
-                return res.render('index',{title:"HomePage"})
+                return res.render('index',{title:"Index Page"})
+        },
+        homePage:function(req,res){
+             return res.render('home',{title:"HomePage"})
         },
         signUpPage:function(req,res){
             return res.render('signup',{title:'Sign Up'})
-        }
+        },
+        
+        postSignUp:passport.authenticate('local.signup',{
+            successRedirect:'/home',
+            failureRedirect:'/signup',
+            failureFlash:true
+        })
     }
 }
