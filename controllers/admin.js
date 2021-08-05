ec517290c3c3626d17a92cfea347ef12637b2fd7
don savinero <donsavinero@gmail.com>
2021-08-05 20:12:01 +0200

    Dashboard design

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,13 @@
+'use strict'
+
 module.exports = function(){
     return{
         setRouting:function(router){
-            
+            router.get('/dashboard',this.adminPage);
+        },
+        
+        adminPage:function(req,res){
+            res.render('admin/dashboard',{title:'Admin DashBoard'})
         }
     }
 }
