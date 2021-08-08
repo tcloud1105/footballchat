9400754817d73f421fbf5dc40cc986398351d085
don savinero <donsavinero@gmail.com>
2021-08-08 20:09:55 +0200

    add createMessage event on client-side

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+    var socket = io();
+    socket.on('connect', function(){
+        console.log('Yeah! User connected');
+    })
     
+    
+    $('#message-form').on('submit', function(e){
+        e.preventDefault();
+        var msg = $('#msg').val();
+    
+        socket.emit("createMessage", {
+            text:msg
+        })
+        
+    })
 })
--- socket/groupchat.js
+++ socket/groupchat.js
@@ -1,3 +1,9 @@
 module.exports = function(io){
-    
+    io.on('connection', (socket)=>{
+        console.log("User connected")
+        
+        socket.on("createMessage", (data)=>{
+            console.log(data);
+        })
+    })
 }
