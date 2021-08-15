54a4b2f27a0769e3057407ccc813b820ba132a9b
don savinero <donsavinero@gmail.com>
2021-08-15 21:38:25 +0200

    message aggregate functionality

--- controllers/privatechat.js
+++ controllers/privatechat.js
@@ -50,7 +50,40 @@ module.exports = function(async,Users, Message){
                          .exec((err, result)=>{
                              callback(err,result)
                        })
-                       }
+                       },
+                  function(callback){
+                    const nameRegex = new RegExp("^" + req.user.username.toLowerCase(), "i")
+                    Message.aggregate(
+                        {$match:{$or:[{"senderName":nameRegex}, {"receiverName":nameRegex}]}},
+                        {$sort:{"createdAt":-1}},
+                        {
+                            $group:{"_id":{
+                            "last_message_between":{
+                                $cond:[
+                                    {
+                                        $gt:[
+                                        {$substr:["$senderName",0,1]},
+                                        {$substr:["$receiverName",0,1]}]
+                                    },
+                                    {$concat:["$senderName"," and ","$receiverName"]},
+                                    {$concat:["$receiverName"," and ","$senderName"]}
+                                ]
+                            }
+                            }, "body": {$first:"$$ROOT"}
+                            }
+                        }, function(err, newResult){
+                            const arr = [
+                                {path: 'body.sender', model: 'User'},
+                                {path: 'body.receiver', model: 'User'}
+                            ];
+                            
+                            Message.populate(newResult, arr, (err, newResult1) => {
+                                callback(err, newResult1);
+                            });
+                        }
+                    )
+                },
+                
                ],(err,results)=>{
                  const result1 = results[0];
                 
